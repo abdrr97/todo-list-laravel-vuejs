b5ae3d1a1862d77ce9136f0f205be811e71eda3d
Abderrahmane <abdrr97@gmail.com>
2021-06-05 13:48:33 +0100

    changing procfileto pascalcase

--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::prefix('/item')->group(function ()
     Route::post('/store', [ItemController::class, 'store']);
     Route::put('/{id}', [ItemController::class, 'update']);
     Route::delete('/{id}', [ItemController::class, 'destroy']);
-});
+}); 
+// test
